1a31366a9346d538374efbb2031cc5043f1ee747
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-04-16 22:57:56 +0300

    lesson 0 - module 7

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,14 +4,12 @@ public class MainTest {
     @Test
     public void  myFirstTest()
     {
-        //double number = 10.0/3.0;
-        int a = 15;
-        int b = 10;
+        int a = 10;
+        int b = 15;
         if(a > b) {
             System.out.println("This will never happen");
         } else {
             System.out.println("This is what will happen");
         }
-        //System.out.println(number);
     }
 }
