88037f93a095852c8290dd20f980c75bf50d12ae
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-04-16 22:51:55 +0300

    Initial commit

---
+++ src/MainTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void  myFirstTest()
+    {
+        //double number = 10.0/3.0;
+        int a = 15;
+        int b = 10;
+        if(a > b) {
+            System.out.println("This will never happen");
+        } else {
+            System.out.println("This is what will happen");
+        }
+        //System.out.println(number);
+    }
+}
