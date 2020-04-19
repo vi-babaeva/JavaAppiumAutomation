1487af84a18aa83b8c4aa7aead9a5a6080f7db2d
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-04-19 14:41:56 +0300

    Тест 1 v.2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,13 +1,13 @@
 import org.junit.Test;
 
 public class MainClass {
-    public int getLocalNumber(int number) {
-        return number * 2;
+    public int getLocalNumber() {
+        return 14;
     }
 
     @Test
     public void testGetLocalNumber() {
-        int a = this.getLocalNumber(7);
+        int a = this.getLocalNumber();
 
         if (a == 14) {
             System.out.println("getLocalNumber return 14 - it's true");
