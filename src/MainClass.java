c68d0749d48b2747d9212fdc881cfdd7dfe304a7
vi-babaeva <viktoriababaeva07@yandex.ru>
2020-04-19 23:07:17 +0300

    Test 3
    dz 3 - lesson 0

--- src/MainClass.java
+++ src/MainClass.java
@@ -16,11 +16,17 @@ public class MainClass {
             System.out.println("Number is not equal to 14");
         }
     }
-    */
+
 
     private int class_number = 20;
 
     public int getClassNumber(){
         return class_number;
+    }
+*/
+    private String class_string = "Hello, world!";
+
+    public String getClassString() {
+        return class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,7 +1,8 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
-
+/*
     @Test
     public void testGetClassNumber() {
         int a = this.getClassNumber();
@@ -11,5 +12,15 @@ public class MainClassTest extends MainClass {
         } else {
             System.out.println("False: the number less or equal to 45");
         }
+    }
+*/
+    @Test
+    public void testGetClassString() {
+        String s = getClassString();
+
+        boolean word1 = s.contains("Hello");
+        boolean word2 = s.contains("hello");
+
+        Assert.assertTrue("This line does not contain the words \"Hello\" or \"hello\" ", word1 || word2);
     }
 }
